f5d4e7bdfdb4c42a22d3fb272734723148744f87
Dmitry Stupin <dm.stup@mail.ru>
2024-10-31 01:50:52 +0700

    Исправление протокола

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const weatherCodes = {
   };
 
 const getWeatherDetails = async (cityName) => {
-    const API_URL = `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${cityName}`;
+    const API_URL = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${cityName}`;
 
     try {
         const response = await fetch(API_URL);
