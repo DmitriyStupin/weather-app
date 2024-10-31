74715da687fab4be29227ceb65632933a90cabfb
Dmitry Stupin <dm.stup@mail.ru>
2024-10-31 10:46:45 +0700

    написание скрипта

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const searchInput = document.querySelector('.search-input');
+const locationButton = document.querySelector('.location-button');
 const currentWeatherDiv = document.querySelector('.current-weather');
+const hourlyWeatherDiv = document.querySelector('.hourly-weather .weather-list');
 
 const API_KEY = "d01a053fb4fb461ea92180440243010";
 
@@ -12,34 +14,77 @@ const weatherCodes = {
     snow: [1066, 1069, 1072, 1114, 1117, 1204, 1207, 1210, 1213, 1216, 1219, 1222, 1225, 1237, 1249, 1252, 1255, 1258, 1261, 1264, 1279, 1282],
     thunder: [1087, 1279, 1282],
     thunder_rain: [1273, 1276]
-  };
+};
 
-const getWeatherDetails = async (cityName) => {
-    const API_URL = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${cityName}`;
+const dispayHourlyForecast = (hourlyData) => {
+    const currentHour = new Date().setMinutes(0, 0, 0);
+    const next24Hours = currentHour + 24 * 60 * 60 * 1000;
 
+    const next24HoursData = hourlyData.filter(({ time }) => {
+        const forecastTime = new Date(time).getTime();
+        return forecastTime >= currentHour && forecastTime <= next24Hours;
+    });
+
+    hourlyWeatherDiv.innerHTML = next24HoursData.map((item) => {
+        const temperature = Math.floor(item.temp_c);
+        const time = item.time.split(" ")[1].substring(0, 5);
+        const weatherIcon = Object.keys(weatherCodes).find(icon => weatherCodes[icon].includes(item.condition.code));
+
+        return `<li class="weather-item">
+                    <p class="time">${time}</p>
+                    <img src="./img/icons/${weatherIcon}.svg" alt="" class="weather-icon" width="28" height="28">
+                    <p class="temperature">${temperature}°</p>
+                </li>`
+    }).join("");
+}
+
+const getWeatherDetails = async (API_URL) => {
+    window.innerWidth <= 768 && searchInput.blur();
+    document.body.classList.remove('show-no-result');
     try {
         const response = await fetch(API_URL);
         const data = await response.json();
 
-        
+
         const temperature = Math.floor(data.current.temp_c);
         const description = data.current.condition.text;
-        const weatherIcon = Object.keys(weatherCodes).find(icon => weatherCodes[icon].includes(data.current.condition.code))
-        
+        const weatherIcon = Object.keys(weatherCodes).find(icon => weatherCodes[icon].includes(data.current.condition.code));
+
         currentWeatherDiv.querySelector('.weather-icon').src = `./img/icons/${weatherIcon}.svg`;
         currentWeatherDiv.querySelector('.temperature').innerHTML = `${temperature}<span>°C</span>`;
         currentWeatherDiv.querySelector('.description').innerText = description;
 
-        console.log(data);
+        const combinedHourlyData = [...data.forecast.forecastday[0].hour, ...data.forecast.forecastday[1].hour];
+        dispayHourlyForecast(combinedHourlyData);
+
+        searchInput.value = data.location.name;
     } catch (error) {
-        console.log(error)
+        document.body.classList.add('show-no-result');
     }
+}
+
+const setupWeatherRequest = (cityName) => {
+    const API_URL = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${cityName}&days=2`;
+    getWeatherDetails(API_URL)
 }
 
 searchInput.addEventListener("keyup", (e) => {
     const cityName = searchInput.value.trim();
 
     if (e.key === "Enter" && cityName) {
-        getWeatherDetails(cityName);
+        setupWeatherRequest(cityName);
     }
 })
+
+locationButton.addEventListener("click", () => {
+    navigator.geolocation.getCurrentPosition(position => {
+        const { latitude, longitude } = position.coords;
+
+        const API_URL = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${latitude},${longitude}&days=2`;
+        getWeatherDetails(API_URL)
+    }, error => {
+        console.log(error);
+    })
+})
+
+setupWeatherRequest('krasnoyarsk')
